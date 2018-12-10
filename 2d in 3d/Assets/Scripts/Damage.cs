6046fc0761f49951656281e3ba56a920f7dcda1f
Malleoluses R <33025211+Tuinsomatic@users.noreply.github.com>
2018-12-10 13:02:25 +0000

    obstacle scripting

---
+++ 2d in 3d/Assets/Scripts/Damage.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Damage : MonoBehaviour {
+
+    public int health = 100;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (health < 0)
+        {
+            Destroy(gameObject);
+        }
+	}
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Obstacle")
+        {
+            health -= 5;
+        }
+        else if (collision.gameObject.tag == "Ground")
+        {
+            health -= 10;
+        }
+    }
+}
